7080bc0e17c6939174959c80cc8d1402f811db3d
fhildeb <felix@lukso.io>
2022-07-05 13:57:53 +0200

    add input types

---
+++ practical-02/01-input-data-types.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+	Praktikum 2: Aufgabe 1
+	Zeichen in verschiedenen Datentypen ausgeben
+	@author: fhildeb1
+*/
+int main()
+{
+	//Eingabe
+	char input;
+	
+	//Solange nicht beendet wird, 
+	//Eingabe umgewandelt anzeigen
+   while(1) 
+   {
+   	
+   	//Startzeile und Einlesen
+      printf("->Druecken sie Eine Taste oder Q zum Beenden: \n");
+      scanf(" %c",&input);
+
+	  //Falls Beenden gewünscht
+      if(input == 'Q')
+      {
+          return EXIT_SUCCESS;
+      }
+      else
+      {
+          //Ausgabe in verschiedenen Datentypen
+          //%c Zeichen
+          //%u unsigned
+          //%x Hexadezimal
+	      printf("Anzeige: '%c' %d %x \n", input, input, input);
+      }	  
+    }
+}                    
