3c2f44558b9df3d7d3b828d3f86ea59f099bdb43
fhildeb <felix@lukso.io>
2022-07-05 21:53:19 +0200

    add simple sema

---
+++ templates/sema/simple/shmclient.c
@@ -0,0 +1,86 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/sem.h>
+#include <sys/shm.h>
+#define SHMSZ 256
+#define NSEMS 1
+
+int main (int argc, char **argv)
+/* Demo für Nutzung des Shared Memory */
+{
+    int shmid, semid;
+    key_t key, semkey;
+    char *shm;
+    
+    struct sembuf ops;
+    
+    if(argc != 3) {
+        fprintf(stderr, "Aufruf: shmclient <servername> <mesg>\n Server wird gestoppt bei mesg 0\n");
+	return EXIT_FAILURE;
+    }
+
+    if((key = ftok(argv[1], '1')) < 0) {
+    	perror("ftok shm");
+    	return EXIT_FAILURE;
+    
+    }
+    
+    if((semkey = ftok(argv[1],'2')) < 0) {
+    	perror("ftok sem");
+    	return EXIT_FAILURE;
+    }
+    
+    /* Shared memory anfordern, Besitzer und Gruppe dürfen lesen und
+       Schreiben, muss existieren (Server up) */
+    if ((shmid = shmget(key, SHMSZ, 0660)) < 0) {
+	perror("shmget");
+	return EXIT_FAILURE;
+    }
+
+    /* dito für Semaphorgruppe mit einem Semaphor */
+    
+    if ((semid = semget(semkey,NSEMS,0660)) <0) {
+	perror("semget");
+	return EXIT_FAILURE;
+    } 
+    
+
+    /* Shared Memory in Adressraum einblenden */
+    if ((shm = shmat(shmid, NULL, 0)) == (char *)(-1)) {
+	perror("shmat");
+	return EXIT_FAILURE;
+    }
+
+
+    /* exklusiven Zugriff auf Shared Memory sichern */
+    ops.sem_num=0;
+    ops.sem_op=-1;
+    ops.sem_flg=0;
+    if(semop(semid, &ops, 1)==-1) {
+	perror("semop P");
+	exit(EXIT_FAILURE);
+    }
+    
+    strncpy(shm, argv[2], SHMSZ);
+
+    /* exklusiven Zugriff wieder freigeben */
+    ops.sem_num=0;
+    ops.sem_op=+1;
+    ops.sem_flg=0;
+    if(semop(semid, &ops, 1)==-1) {
+	perror("semop P");
+	exit(EXIT_FAILURE);
+    }
+     
+    /* Shared Memory abkoppeln */
+    if(shmdt(shm)<0) {
+    	perror("shmdt");
+	return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
+}
+
---
+++ templates/sema/simple/shmserver.c
@@ -0,0 +1,154 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <assert.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/sem.h>
+#include <sys/shm.h>
+#include <signal.h>
+#define SHMSZ 256
+#define NSEMS 1
+
+int shmid;
+int semid;
+
+/* bei Programmende werden SHM-Segment und Sema gelöscht */
+void exithandler()
+{
+
+   if(shmctl(shmid, IPC_RMID, NULL) < 0) {
+     perror("msgctl");
+     _exit(EXIT_FAILURE);
+   }
+
+   if(semctl(semid, 0, IPC_RMID, NULL) < 0) {
+     perror("semctl");
+     _exit(EXIT_FAILURE);
+   }
+
+   return;
+}
+
+/* bei SIGINT werden SHM-Segment und Sema gelöscht */
+void sighandler(int signo)
+{
+   exithandler();
+   _exit(EXIT_FAILURE);
+}
+
+
+int main (int argc, char **argv)
+/* Demo für Nutzung des Shared Memory,
+   Zugriffssteuerung mit Semaphoren */
+{
+    key_t key, semkey;
+    char *shm;
+    struct sigaction old, new;
+    struct sembuf sops;
+    
+    if(argc != 1) {
+        fprintf(stderr, "Aufruf: shmserver \n");
+	return EXIT_FAILURE;
+    }
+
+    /* Signalbehandlung vorbereiten */
+    sigemptyset(&(new.sa_mask));
+    new.sa_handler = sighandler;
+    new.sa_flags = 0;
+
+    if(sigaction(SIGINT, &new, &old) < 0) {
+	perror("sigaction");
+	exit(EXIT_FAILURE);
+    }
+
+    key = ftok(argv[0], '1');
+    assert(key != -1);
+    semkey = ftok(argv[0], '2');
+    assert(semkey != -1);
+    /* Shared memory anfordern, Besitzer und Gruppe dürfen lesen und
+       Schreiben */
+    if ((shmid = shmget(key, SHMSZ, 0660|IPC_CREAT)) < 0) {
+	perror("shmget");
+	return EXIT_FAILURE;
+    }
+    
+    /* Semaphor anfordern */
+    if ((semid = semget(semkey, NSEMS, 0660|IPC_CREAT)) < 0) {
+	perror("semget");
+	return EXIT_FAILURE;    
+    }
+    
+    /* Exit-Handler installieren, damit SHM in jedem Falle freigegeben wird */
+    atexit(exithandler);
+
+    /* Semaphor initialisieren */
+    if (semctl(semid, 0, SETVAL, 1) < 0) {
+            perror("semctl/seminit");
+            return EXIT_FAILURE;
+    }
+                        
+
+    /* Shared Memory in Adressraum einblenden */
+    if ((shm = shmat(shmid, NULL, 0)) == (char *)(-1)) {
+	perror("shmat");
+	return EXIT_FAILURE;
+    }
+
+    /* exklusiven Zugriff auf Shared Memory sichern */
+    sops.sem_num=0;
+    sops.sem_op=-1;
+    sops.sem_flg=0;
+    if(semop(semid, &sops, 1)==-1) {
+	perror("semop P");
+	exit(EXIT_FAILURE);
+    }
+
+    /* maximal SHMSZ Bytes in SHM kopieren */
+    strncpy(shm, "Hello World!", SHMSZ);
+
+    /* Zeichenkette in SHM ausgeben */
+    while(shm[0] != '0') {
+    	puts(shm);
+    	/* exklusiven Zugriff wieder freigeben */
+    	sops.sem_num=0;
+    	sops.sem_op=+1;
+    	sops.sem_flg=0;
+    	if(semop(semid, &sops, 1)==-1) {
+		perror("semop P");
+		exit(EXIT_FAILURE);
+    	}
+
+	sleep(1);
+	
+	/* exklusiven Zugriff auf Shared Memory sichern */
+    	sops.sem_num=0;
+    	sops.sem_op=-1;
+    	sops.sem_flg=0;
+    	if(semop(semid, &sops, 1)==-1) {
+		perror("semop P");
+		exit(EXIT_FAILURE);
+    	}
+
+    }
+
+    /* exklusiven Zugriff wieder freigeben */
+    sops.sem_num=0;
+    sops.sem_op=+1;
+    sops.sem_flg=0;
+    if(semop(semid, &sops, 1)==-1) {
+	perror("semop P");
+	exit(EXIT_FAILURE);
+    }
+     
+    if(shmdt(shm)<0) {
+    	perror("shmdt");
+	return EXIT_FAILURE;
+    }
+    
+    /* SHM Löschen in Exit-Funktion! */
+    return EXIT_SUCCESS;
+}
+
+
